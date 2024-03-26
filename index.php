d31f445ee3cf0e6a42cee80a0d1612f237eeb535
dantas-726 <pphd10@gmail.com>
2024-03-26 15:29:05 -0300

    first commit

--- index.php
+++ index.php
@@ -2,16 +2,30 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+use Hcode\PageAdmin;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
+
+	$page = new Page();
+	$page->setTpl("index");
     
-	echo "OK";
+
+});
+
+$app->get('/admin', function() {
+
+	$page = new PageAdmin();
+	$page->setTpl("index");
+    
 
 });
 
 $app->run();
 
- ?>
+ 
